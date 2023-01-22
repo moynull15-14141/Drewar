be0918aaa8eeceb9a97686a38fa41aa775750ada
Md.Moynull Hasan <moynull15-14141@diu.edu.bd>
2023-01-22 16:23:50 +0600

    this is new

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,120 @@ class Tst1 extends StatelessWidget {
           drawer: Drawer(
             backgroundColor: const Color.fromARGB(255, 39, 3, 45),
             child: Container(
-              margin: const EdgeInsets.fromLTRB(20, 15, 40, 70),
+              margin: const EdgeInsets.fromLTRB(25, 30, 40, 70),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Rifa",
+                    "E-Mail",
                     style: TextStyle(color: Colors.white, fontSize: 35),
                   ),
                   Text(
                     "Somthing New",
                     style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
-                  Icon(
-                    Icons.email,
-                    color: Colors.white,
-                    size: 80,
-                  )
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'Profile',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 112,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 15,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'E-mail',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 113,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 15,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'Send',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 125,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 15,
+                      )
+                    ],
+                  ),
+                  SizedBox(),
+                  // ListTile(
+                  //   contentPadding:
+                  //       EdgeInsets.symmetric(horizontal: 0, vertical: 40),
+                  //   leading: Icon(
+                  //     Icons.person,
+                  //     color: Colors.white,
+                  //     size: 35,
+                  //   ),
+                  //   title: Text(
+                  //     'Profile',
+                  //     style: TextStyle(color: Colors.white, fontSize: 20),
+                  //   ),
+                  //   trailing: Icon(
+                  //     Icons.arrow_forward,
+                  //     color: Colors.white,
+                  //     size: 25,
+                  //   ),
+                  // ),
                 ],
               ),
             ),
